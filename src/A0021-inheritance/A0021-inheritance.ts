aeea4a05bba6a9539717fb3bf9e31b8a6582f8bf
AdrianoBaza <bazafordev@gmail.com>
2023-11-06 14:45:31 -0300

    Add file A0021-inheritance.ts; Concept POO: Class, Inheritance, Access Modifiers, Polymorphism

---
+++ src/A0021-inheritance/A0021-inheritance.ts
@@ -0,0 +1,40 @@
+export class Pessoa {
+  constructor(
+    public nome: string,
+    public sobrenome: string,
+    private idade: number,
+    protected cpf: string
+    ) {}
+
+    getIdade(): number {
+      return this.idade;
+    }
+
+    getCpf(): string {
+      return this.cpf;
+    }
+
+    getNomeCompleto(): string {
+      return this.nome + ' ' + this.sobrenome;
+    }
+}
+
+export class Aluno extends Pessoa {
+  getNomeCompleto(): string {
+    return 'Isso vem do aluno: ' + this.nome + ' ' + this.sobrenome;
+  }
+}
+
+export class Cliente extends Pessoa {
+  getNomeCompleto(): string {
+    return 'Isso vem do cliente: ' + this.nome + ' ' + this.sobrenome;
+  }
+}
+
+const pessoa = new Pessoa('Adriano', 'Baza', 49, '000.000.000-00');
+const aluno = new Aluno('Vitor', 'Ferreira', 12, '111.111.111-11');
+const cliente = new Cliente('Ana', 'Catarina', 46, '222.222.222-22');
+
+console.log(pessoa.getNomeCompleto());
+console.log(aluno.getNomeCompleto());
+console.log(cliente.getNomeCompleto());
